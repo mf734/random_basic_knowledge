b39f24716836663b20f64a43ee8f885c9541095c
Manquan Fang <42682039+mf734@users.noreply.github.com>
2019-09-22 14:07:58 -0400

    Add files via upload

---
+++ example.java
@@ -0,0 +1,17 @@
+//循环
+for (int i = 0; i < nums.length; i++) {}
+// 返回数组
+return new int[] {i, j};
+// 无效输入
+throw new IllegalArgumentException
+
+
+// map的方法
+Map<Integer, Integer> map = new HashMap<>();
+// 加一个，前者为key后者为value
+map.put(nums[i], i);
+// 检查是否含有n的key
+map.containsKey(n);
+// 取出key为i的值
+map.get(i);
+
